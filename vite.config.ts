184a27bf760f4d6b9ede7b7d001e7b49af1f2ebe
Manan-Wadhwa <wadhwamanan11@outlook.com>
2026-06-13 22:38:40 +0530

    removed pre-renderer

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import vitePrerender from 'vite-plugin-prerender'
-import path from 'path'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react(),
-    vitePrerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: ['/'],
-    }),
   ],
 })
